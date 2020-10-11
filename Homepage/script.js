56ed61566681f0ab0f12426c559fe0b2d09f4253
Joas Boevink <joas.boevink@kpnmail.nl>
2020-10-11 20:13:27 +0200

    add project cards and socials

--- Homepage/script.js
+++ Homepage/script.js
@@ -1,1 +1,23 @@
-//Javascript
+const mail = document.getElementById("mailInput");
+const discord = document.getElementById("discordInput");
+const tooltip = document.getElementById("myTooltip");
+const tooltipD = document.getElementById("myTooltipD");
+
+function copyMail() {
+    mail.select();
+    mail.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    tooltip.innerHTML = "Copied Mail! ";
+}
+
+function copyDiscord() {
+    discord.select();
+    discord.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    tooltipD.innerHTML = "Copied Discord!";
+}
+
+function outFunc() {
+    tooltip.innerHTML = "Copy to clipboard";
+    tooltipD.innerHTML = "Copy to clipboard";
+  }
